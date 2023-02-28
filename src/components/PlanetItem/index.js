5d40ada041a71f6e05d1e06739388d61e605f461
M4adhavgit <v9543320@gmail.com>
2023-02-28 08:40:15 +0000

    planets App

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,19 @@
+import {
+  PlanetItemContainer,
+  ImagePlanet,
+  NamePlanet,
+  DescriptionPlanet,
+} from './styledComponents'
+
+const PlanetItem = props => {
+  const {eachValue} = props
+  return (
+    <PlanetItemContainer>
+      <ImagePlanet src={eachValue.imageUrl} alt={`planet ${eachValue.name}`} />
+      <NamePlanet>{eachValue.name}</NamePlanet>
+      <DescriptionPlanet>{eachValue.description}</DescriptionPlanet>
+    </PlanetItemContainer>
+  )
+}
+
+export default PlanetItem
